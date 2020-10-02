eac84bb3fdd3ab615ca5d49176549841eab8a55a
ret <ivan2011_98@mail.ru>
2020-10-02 16:18:33 +0300

    Test Program

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static int func(int t){
+        int n = 4;
+        int h = n + t * 5;;
+        return n;
+    }
+    public static void main(String[] args) {
+	// write your code here
+        int x = func(7);
+        System.out.print(x);
+    }
+
+
+}
